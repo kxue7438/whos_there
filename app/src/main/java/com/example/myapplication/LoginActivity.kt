9a5511389e66c25026373c9523387cb87c088701
2020vbo <victorbo2020@gmail.com>
2022-05-07 19:56:29 -0400

    Added user login and registration via email/password with Firebase Authentication

---
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -0,0 +1,77 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var userEmail: EditText
+    private lateinit var userPassword: EditText
+    private lateinit var loginBtn: Button
+    private lateinit var goReg: Button
+    private lateinit var progressBar: ProgressBar
+    private var mAuth: FirebaseAuth? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        initializeUI()
+
+        loginBtn.setOnClickListener { loginUserAccount() }
+
+        goReg.setOnClickListener { startActivity(Intent(this@LoginActivity, RegistrationActivity::class.java)) }
+    }
+
+    private fun loginUserAccount() {
+        progressBar.visibility = View.VISIBLE
+
+        val email: String = userEmail.text.toString()
+        val password: String = userPassword.text.toString()
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(applicationContext, "Please enter email...", Toast.LENGTH_LONG).show()
+            return
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(applicationContext, "Please enter password!", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        mAuth!!.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                progressBar.visibility = View.GONE
+                if (task.isSuccessful) {
+                    Toast.makeText(applicationContext, "Login successful!", Toast.LENGTH_LONG)
+                        .show()
+                    startActivity(Intent(this@LoginActivity, MapsActivity::class.java))
+                } else {
+                    Toast.makeText(
+                        applicationContext,
+                        "Login failed! Please try again later",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+    }
+
+    private fun initializeUI() {
+        userEmail = findViewById(R.id.email)
+        userPassword = findViewById(R.id.password)
+
+        loginBtn = findViewById(R.id.login)
+        progressBar = findViewById(R.id.progressBar)
+
+        goReg = findViewById(R.id.goto_register)
+    }
+}
